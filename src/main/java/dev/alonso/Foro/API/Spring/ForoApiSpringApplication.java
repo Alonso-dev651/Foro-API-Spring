d980f0e8c5074a38728300478ef4e9c067be37b4
AlonsoDev <marcialpebell@gmail.com>
2025-02-10 18:22:50 -0500

    First commit

---
+++ src/main/java/dev/alonso/Foro/API/Spring/ForoApiSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.alonso.Foro.API.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alonso/Foro/API/Spring/ForoApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alonso.Foro.API.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
